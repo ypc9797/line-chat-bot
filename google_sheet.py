166f599734349126918126ab3988dd88b0d5e3a0
anna <yp.c9797@gmail.com>
2018-05-31 03:27:31 +0800

    hi

--- google_sheet.py
+++ google_sheet.py
@@ -10,15 +10,17 @@ def update_sheet(gss_client, key, item,the_day):
 	sheet = wks.sheet1
 	sheet.insert_row([item,the_day], 2)
 def find_user_period(gss_client, key, userid):
+	print("窩要近來查了")
+	print(userid)
 	wks = gss_client.open_by_key(key)
 	sheet = wks.sheet1
 	try:
+		print("我查到了")
 		cell = sheet.find(userid)
 		r = cell.row
 		c = cell.col
 		period_day = sheet.cell(r,c+1).value
-		print('hihihihihi')
-		print(period_day)
 		return period_day
 	except:
+		print("我查沒有")
 		return 0
