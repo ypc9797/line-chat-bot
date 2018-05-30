6208f7a9626efd625c0fb219bb2118c65fe40019
anna <yp.c9797@gmail.com>
2018-05-31 03:15:42 +0800

    hi

--- app.py
+++ app.py
@@ -433,28 +433,31 @@ def handle_postback(event):
 
 
 	elif event.postback.data == 'cal':
+		print("我是三")
 		the_id = event.source.user_id
 		profile = line_bot_api.get_profile(the_id)
 		user_name = profile.display_name
-		
+		print("我是四")
 		auth_json_path = 'google_sheet.json'
 		gss_scopes = ['https://spreadsheets.google.com/feeds']
 		gss_client = google_sheet.auth_gss_client(auth_json_path, gss_scopes)
 		spreadsheet_key = '1Q4hWEVjTB-rdc7HAi_Yc_cF4uymjfPHe70Cc36fLHyM'
-
+		print("我是五")
 		try:
 			print('im here!!')
 			last_date_str = find_user_period(gss_client, spreadsheet_key, the_id)
 			print(last_date_str)
 		except:
+			print("我是六")
 			line_bot_api.reply_message(event.reply_token, TextSendMessage(text='你還沒有輸入最近的一次月經日期歐～請輸入"第一次"來選擇吧!!'))
 		
+		print("我是七")
 		last_date = datetime.strftime(last_date_str,"%Y-%m-%d")		
 		period = last_date + timedelta(days=28) #月經日期是第一天加上週期
 		preg = last_date + timedelta(days=10) #經期來後10天開始的一周內容易懷孕
 		diet = last_date + timedelta(days=7) #經期來後7天開始的一周內容易懷孕
 		bra = last_date + timedelta(days=17)
-
+		print("我是八")
 		content = ''
 		content += user_name + ' 你好~\n'
 		content += '你上次紀錄的日期是' + last_date_str + '\n\n'
@@ -463,6 +466,7 @@ def handle_postback(event):
 		content += diet.strftime("%m/%d") + ' 開始的一週內少吃多動會瘦很快!!\n\n'
 		content += bra.strftime("%m/%d") + ' 開始的一週多按摩奶奶會長很大唷\n\n'
 		content += '輸入"科普"來補充更多小知識吧 (✿╹◡╹)'
+		print("我是九")
 		line_bot_api.reply_message(event.reply_token, TextSendMessage(text=content))
 
 
